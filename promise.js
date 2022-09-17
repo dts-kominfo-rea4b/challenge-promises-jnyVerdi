7c1682c88633976997f081e3877ea2ccd492c2c8
Jenny Verdi <jnyverdi@gmail.com>
2022-09-17 07:28:07 +0700

    finishing function of 'promiseOutput' and make sure the test test is passed

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async emotion => {
+  try {
+    const IXX = await promiseTheaterIXX();
+    const VGC = await promiseTheaterVGC();
+
+    return [...IXX, ...VGC].filter(element => element.hasil === emotion).length;
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 module.exports = {
   promiseOutput,
